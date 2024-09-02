a911e26441fc59402056a513d71c684ae4d4f09d
yaman3108 <133077812+yaman3108@users.noreply.github.com>
2024-09-02 16:22:48 +0530

    Create EXP13.2.cpp

---
+++ EXP13.2.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+class room{
+private:
+double length;
+double bredth;
+
+public:
+room(){
+  length=5.5;
+  breadth=4.4;
+}
+room(double l, double b){
+  length=l;
+  breadth=b;
+}
+};
+int main()
+{
