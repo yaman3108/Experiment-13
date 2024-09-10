169e1ce1d5552ac248ac9aacdb64c4b8cd35ffc4
yaman3108 <133077812+yaman3108@users.noreply.github.com>
2024-09-10 14:47:51 +0530

    Update EXP13.2.cpp

--- EXP13.2.cpp
+++ EXP13.2.cpp
@@ -1,19 +1,40 @@
-#include<iostream>
+# include<iostream>
 using namespace std;
-class room{
-private:
-double length;
-double bredth;
 
-public:
-room(){
-  length=5.5;
-  breadth=4.4;
-}
-room(double l, double b){
-  length=l;
-  breadth=b;
-}
+class room
+{
+    private:
+    double l,b;
+
+    public:
+    room()
+    {
+        l = 1.2;
+        b = 2.3;
+    }
+    room(double len, double bre)
+    {
+        l = len;
+        b = bre;
+    }
+    room(double len)
+    {
+        l = len;
+        b = 4.5;
+    }
+    double area()
+    {
+        return l*b;
+    }
+
 };
 int main()
 {
+    room r1,r2(6.7,7.8),r3(9.1);
+    cout<<"No parameter passed: "<<endl;
+    cout<<"Area: "<<r1.area()<<endl;
+    cout<<"Two parameters passed: "<<endl;
+    cout<<"Area: "<<r2.area()<<endl;
+    cout<<"One parameter passed: "<<endl;
+    cout<<"Area: "<<r3.area()<<endl;
+}
